fd01988682cb70c2d683bc5d57d4bf4cef7cfadc
Reazul Islam <reazulislam137@gmail.com>
2018-03-08 21:56:18 +0600

    first commit

---
+++ march-8-01.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a = 125, b = 12345;
+    long ax = 1234567890;
+    short s = 4043;
+    float x = 2.13459;
+    double dx = 1.6;
+    char c = 'W';
+    unsigned long ux = 2541567890;
+
+    printf("a + c = %d\n",a+c);
+    printf("x + c = %fd\n", x+c);
+    printf("dx + x = %ld\n", dx+x);
+    printf("((int) dx) + ax = %ld\n", ((int) dx)+ax);
+    printf("a + x = %fd\n",a+x);
+    printf("s + b = %d\n",s+b);
+    printf("ax+b = %ld\n",ax+b);
+    printf("s+c = %hd\n",s+c);
+    printf("ax+c = %ld\n",ax+c);
+    printf("ax+ux = %lu\n",ax+ux);
+
+
+    return 0;
+}
